e2abaa9a6271ded902c44a9a2209e5ef228e1f00
李红力 <honglili@sohu-inc.com>
2016-04-22 10:24:44 +0800

    Initial Commit

---
+++ EmojiDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EmojiDemo
+//
+//  Created by LHL on 16/4/22.
+//  Copyright © 2016年 LHL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
